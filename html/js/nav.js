28b74ec45cb6dabf33f2506faeff2e234b90691f
wkuglen <wkuglen@gmail.com>
2018-12-09 17:40:21 -0600

    Add home button to nav bar

--- html/js/nav.js
+++ html/js/nav.js
@@ -1,2 +1,2 @@
 document.getElementById('idNav').insertAdjacentHTML('beforeend',
-'<h1><a href="../../interventions/low/index.html" class="navLow">Low</a></h1> <h1><a href="../../interventions/mid/01.html" class="navMid">Mid</a></h1> <h1><a href="../../interventions/high/01.html" class="navHigh">High</a></h1>')
+'<h1><a href="../../index.html" style="color: white; background: linear-gradient(to bottom, #ffff00 0%, #ff0000 100%)">home</a></h1> <h1><a href="../../interventions/low/index.html" class="navLow">Low</a></h1> <h1><a href="../../interventions/mid/01.html" class="navMid">Mid</a></h1> <h1><a href="../../interventions/high/01.html" class="navHigh">High</a></h1>')
