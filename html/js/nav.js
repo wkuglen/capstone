d9ee79bdd84a094087cc480b0693bc4f3322456a
wkuglen <wkuglen@gmail.com>
2018-12-09 16:07:22 -0600

    Add nav.js to auto build nav bar

---
+++ html/js/nav.js
@@ -0,0 +1,2 @@
+document.getElementById('idNav').insertAdjacentHTML('beforeend',
+'<h1><a href="../../interventions/low/index.html" class="navLow">Low</a></h1> <h1><a href="../../interventions/mid/01.html" class="navMid">Mid</a></h1> <h1><a href="../../interventions/high/01.html" class="navHigh">High</a></h1>')
