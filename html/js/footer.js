e07e82d618adcb52faa391c0c3a1ad84074e817b
wkuglen <wkuglen@gmail.com>
2019-03-28 12:01:24 -0500

    Add footer and link to wkuglen.com

---
+++ html/js/footer.js
@@ -0,0 +1,2 @@
+document.getElementById('idFooter').insertAdjacentHTML('beforeend',
+'Copyright © 2018-2019 <a href="http://wkuglen.com" target="_blank" class="outbound">Will Kuglen</a>. Screens & Time, featured in the <a href="http://futuretense.design" target="_blank" class="outbound">Future Tense</a> exhibition.')
